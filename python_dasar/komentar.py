99f1eb2209fabf1c2c878181d9e494928b6b4ba2
purwanto1337 <purwanto1337@gmail.com>
2015-08-26 23:38:14 +0700

    Create komentar.py

---
+++ python_dasar/komentar.py
@@ -0,0 +1,17 @@
+#Ini adalah komentar
+# Tulisan ini tidak akan dieksekusi
+
+#komentar dengan tanda pagar hanya bisa digunakan
+#untuk
+#satu
+#baris
+
+print("Hello World") #ini juga komentar
+
+# komentar bisa berisi spesial karakter !@#$%^&*(),./;'[]\
+
+#mencetak nama
+print("Budi")
+
+#mencetak angka/integer
+print(123)
