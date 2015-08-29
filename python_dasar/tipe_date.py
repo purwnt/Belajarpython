573633cfe52d336f930c933a8f0bafea495029fc
purwanto1337 <purwanto1337@gmail.com>
2015-08-30 01:14:12 +0700

    Update tipe_date.py

--- python_dasar/tipe_date.py
+++ python_dasar/tipe_date.py
@@ -1,16 +1,34 @@
+#tipe data Boolean
+print(True)
 
+#tipe data String
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
 
+#tipe data Integer
+print(20)
 
-Boolean	True atau False	Menyatakan benar(True) yang bernilai 1, atau salah(False) yang bernilai 0
-String	"Ayo belajar Python"	Menyatakan karakter/kalimat bisa berupa huruf angka, dll (diapit tanda " atau ')
-Integer	25 atau 1209	Menyatakan bilangan bulat
-Long	12367471224L	Menyatakan bilangan bulat yang sangat panjang
-Float	3.14 atau 0.99	Menyatakan bilangan yang mempunyai koma
-Hexadecimal	9a atau 1d3	Menyatakan bilangan dalam format heksa (bilangan berbasis 16)
-Date	27-08-2015	Menyatakan kalender yang dimulai dari tanggal, bulan dan tahun
-Complex	1 + 5j	Menyatakan pasangan angka real dan imajiner
-List	['xyz', 786, 2.23]	Data untaian yang menyimpan berbagai tipe data dan isinya bisa diubah-ubah
-Tuple	('xyz', 768, 2.23)	Data untaian yang menyimpan berbagai tipe data tapi isinya tidak bisa diubah
-Dictionary	{'nama': 'budi','kode':112}	Data untaian yang menyimpan berbagai tipe data berupa pasangan penunjuk dan nilai
-Object		Sebuah data yang berisi atribut dan method
-None		Menyatakan bahwa tidak terdefinisi tipe data apapun
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9a)
+
+#tipe data Complex
+print(5j)
+
+#tipe data List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+#tipe data Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#tipe data Dictionary
+print({"nama":"Budi", 'umur':20})
+#tipe data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"Andi", 'umur':21} #proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+type(biodata) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'> yang berarti dict adalah tipe data dictionary
+
