ac49583e9012cce51f7356e3bbd076fb10097649
purwanto1337 <purwanto1337@gmail.com>
2015-08-30 03:03:23 +0700

    Update variabel.py

--- python_dasar/variabel.py
+++ python_dasar/variabel.py
@@ -1,1 +1,30 @@
+#proses memasukan data ke dalam variabel
+nama = "John Doe"
+#proses mencetak variabel
+print(nama)
 
+#nilai dan tipe data dalam variabel  dapat diubah
+umur = 20               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "dua puluh satu" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namaDepan = "Budi"
+namaBelakang = "Susanto"
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "Berenang"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye" 
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
