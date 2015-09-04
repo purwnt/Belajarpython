6e8244ec497a3ab97f5fad8c6196e15504e162ea
purwanto1337 <purwanto1337@gmail.com>
2015-09-04 16:32:32 +0700

    Update operator.py

--- python_dasar/operator.py
+++ python_dasar/operator.py
@@ -1,1 +1,17 @@
+Penjumlahan+	1 + 3 = 4	Menjumlahkan nilai dari masing-masing operan atau bilangan
+Pengurangan -	4 - 1 = 3	Mengurangi nilai operan di sebelah kiri menggunakan operan di sebelah kanan
+Perkalian *	2 * 4 = 8	Mengalikan operan/bilangan
+Pembagian /	10 / 5 = 2	Untuk membagi operan di sebelah kiri menggunakan operan di sebelah kanan
+Sisa Bagi%	11 % 2 = 1	Mendapatkan sisa pembagian dari operan di sebelah kiri operator ketika dibagi oleh operan di sebelah kanan
+Pangkat **	8 ** 2 = 64	Memangkatkan operan disebelah kiri operator dengan operan di sebelah kanan operator
+Pembagian Bulat //	10 // 3 = 3	Sama seperti pembagian. Hanya saja angka dibelakang koma dihilangkan
 
+#OPERATOR ARITMATIKA
+
+#Penjumlahan
+apel = 7
+jeruk = 9
+buah = apel + jeruk #
+print(buah)
+
+#Pengurangan
