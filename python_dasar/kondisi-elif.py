9666efa3e13a158bbced23b4349435db95490b12
Purwanto <purwanto1337@gmail.com>
2017-01-29 03:00:48 +0700

    Create kondisi-elif.py

---
+++ python_dasar/kondisi-elif.py
@@ -0,0 +1,18 @@
+#Contoh penggunaan kondisi elif
+
+hari_ini = "Minggu"
+
+if(hari_ini == "Senin"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Selasa"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Kamis"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Jumat"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Sabtu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Minggu"):
+    print("Saya akan libur")
