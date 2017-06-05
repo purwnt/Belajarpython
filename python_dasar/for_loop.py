488d4e019eda56d2ea7423a465e9977b650a6b29
Purwanto <purwanto1337@gmail.com>
2017-06-05 16:42:56 +0700

    Create for_loop.py

---
+++ python_dasar/for_loop.py
@@ -0,0 +1,9 @@
+#Contoh pengulangan for sederhana
+angka = [1,2,3,4,5]
+for x in angka:
+    print(x)
+
+#Contoh pengulangan for
+buah = ["nanas", "apel", "jeruk"]
+for makanan in buah:
+    print("Saya suka makan", makanan)
