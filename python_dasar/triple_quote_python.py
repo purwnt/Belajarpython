731a86e22fae8c6e1375bd95fbaabc2027987086
Purwanto <purwanto1337@gmail.com>
2017-06-12 11:20:54 +0700

    Create triple_quote_python.py

---
+++ python_dasar/triple_quote_python.py
@@ -0,0 +1,8 @@
+kutipantiga = """this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( \t ) and they will show up that way when displayed.
+NEWLINEs within the string, whether explicitly given like
+this within the brackets [ \n ], or just a NEWLINE within
+the variable assignment will also show up.
+"""
+print (kutipantiga)
