460577119334d5b80413a6f1ba59bfc37914ddfb
Purwanto <purwanto1337@gmail.com>
2017-06-14 14:55:59 +0700

    Create list_python_sederhana.py

--- python_dasar/list_python_sederhana.py
+++ python_dasar/list_python_sederhana.py
@@ -1,3 +1,4 @@
-list1 = ['kimia', 'fisika', 1993, 2017];
-list2 = [1, 2, 3, 4, 5 ];
-list3 = ["a", "b", "c", "d"];
+#Contoh sederhana pembuatan list pada bahasa pemrograman python
+list1 = ['kimia', 'fisika', 1993, 2017]
+list2 = [1, 2, 3, 4, 5 ]
+list3 = ["a", "b", "c", "d"]
