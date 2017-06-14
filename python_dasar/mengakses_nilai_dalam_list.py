79902c33dd782386618b9d732c8952e82d38eac6
Purwanto <purwanto1337@gmail.com>
2017-06-14 15:00:30 +0700

    Create mengakses_nilai_dalam_list.py

---
+++ python_dasar/mengakses_nilai_dalam_list.py
@@ -0,0 +1,7 @@
+#Cara mengakses nilai di dalam list Python
+
+list1 = ['fisika', 'kimia', 1993, 2017]
+list2 = [1, 2, 3, 4, 5, 6, 7 ]
+
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
