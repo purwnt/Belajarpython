d40cedff54ec3638c75f30d3752d7f9edeb12375
Purwanto <purwanto1337@gmail.com>
2017-06-15 14:19:59 +0700

    Create update_dictionary_python.py

---
+++ python_dasar/update_dictionary_python.py
@@ -0,0 +1,8 @@
+#Update dictionary python
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # Mengubah entri yang sudah ada
+dict['School'] = "DPS School" # Menambah entri baru
+
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
