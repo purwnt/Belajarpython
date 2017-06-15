a92e6295450634e2270af05b897dcae8cb4c37dd
Purwanto <purwanto1337@gmail.com>
2017-06-15 11:40:50 +0700

    Create update_nilai_tuple_python.py

---
+++ python_dasar/update_nilai_tuple_python.py
@@ -0,0 +1,10 @@
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
+
+# Aksi seperti dibawah ini tidak bisa dilakukan pada tuple python
+# Karena memang nilai pada tuple python tidak bisa diubah
+# tup1[0] = 100;
+
+# Jadi, buatlah tuple baru sebagai berikut
+tup3 = tup1 + tup2
+print (tup3)
