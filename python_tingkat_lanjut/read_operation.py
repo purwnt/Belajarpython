a910c10612236ee7318eacbc5b6f07d5ddad59c7
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:54:17 +0700

    Create read_operation.py

---
+++ python_tingkat_lanjut/read_operation.py
@@ -0,0 +1,30 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > '%d'" % (1000)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      lname = row[1]
+      age = row[2]
+      sex = row[3]
+      income = row[4]
+      # Now print fetched result
+      print ("fname = %s,lname = %s,age = %d,sex = %s,income = %d" % \
+             (fname, lname, age, sex, income ))
+except:
+   print ("Error: unable to fetch data")
+
+# disconnect from server
+db.close()
