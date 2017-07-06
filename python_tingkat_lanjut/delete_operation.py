93986dab91fb20bd9952d43f5f9727205fb32d5d
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:57:37 +0700

    Create delete_operation.py

---
+++ python_tingkat_lanjut/delete_operation.py
@@ -0,0 +1,21 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to DELETE required records
+sql = "DELETE FROM EMPLOYEE WHERE AGE > '%d'" % (20)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
