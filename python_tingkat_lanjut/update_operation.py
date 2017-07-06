3bf71bd962f2ed12125e40dbac8180e270c94e69
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:56:22 +0700

    Create update_operation.py

---
+++ python_tingkat_lanjut/update_operation.py
@@ -0,0 +1,22 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to UPDATE required records
+sql = "UPDATE EMPLOYEE SET AGE = AGE + 1
+                          WHERE SEX = '%c'" % ('M')
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
