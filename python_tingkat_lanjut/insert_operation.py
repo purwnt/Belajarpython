e59a5c99a20136fbbfbc185e33701e9646433a1e
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:25:31 +0700

    Create insert_operation.py

---
+++ python_tingkat_lanjut/insert_operation.py
@@ -0,0 +1,23 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+   LAST_NAME, AGE, SEX, INCOME)
+   VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
