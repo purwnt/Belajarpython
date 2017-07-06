eb7ddef5d994b989fc55996a5497029c4fefb231
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:21:05 +0700

    Create koneksi_database.py

---
+++ python_tingkat_lanjut/koneksi_database.py
@@ -0,0 +1,18 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print ("Database version : %s " % data)
+
+# disconnect from server
+db.close()
