73864a41a7da5cf789b91ce30e08315684fb3649
Purwanto <purwanto1337@gmail.com>
2017-07-06 18:22:35 +0700

    Create create_table_database.py

---
+++ python_tingkat_lanjut/create_table_database.py
@@ -0,0 +1,23 @@
+import PyMySQL
+
+# Open database connection
+db = PyMySQL.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+# Create table as per requirement
+sql = """CREATE TABLE EMPLOYEE (
+   FIRST_NAME  CHAR(20) NOT NULL,
+   LAST_NAME  CHAR(20),
+   AGE INT,  
+   SEX CHAR(1),
+   INCOME FLOAT )"""
+
+cursor.execute(sql)
+
+# disconnect from server
+db.close()
