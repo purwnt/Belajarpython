4af92ca40cbeefec4316079e1e8f3e149ffdc206
Purwanto <purwanto1337@gmail.com>
2017-11-03 08:50:32 +0700

    Perbaikan kurang buka tutup kurung

--- python_dasar/nested_loop.py
+++ python_dasar/nested_loop.py
@@ -6,7 +6,7 @@ while(i < 100):
     while(j <= (i/j)):
         if not(i%j): break
         j = j + 1
-    if (j > i/j) : print i, " is prime"
+    if (j > i/j) : print(i, " is prime")
     i = i + 1
 
 print "Good bye!"
